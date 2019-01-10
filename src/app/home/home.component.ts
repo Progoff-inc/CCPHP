b759816157f506e8105610b967bba9ead4b7c6a3
IANomokonov <nomokonov.vana@yandex.ru>
2019-01-11 02:04:58 +0300

    Делаю шапку первой страницы

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  searchForm: FormGroup;
+  search:Search = new Search();
+  constructor(private formBuilder: FormBuilder){
+
+  }
+  get f() { return this.searchForm.controls; }
+  ngOnInit(){
+    this.searchForm = this.formBuilder.group({
+      Pick: ['', Validators.required],
+      Drop: ['', Validators.required]     
+    });
+  }
+}
+
+export class Search{
+  Pick:string;
+  Drop:string;
+  DateStart:Date;
+  DateFinish:Date;
 }
