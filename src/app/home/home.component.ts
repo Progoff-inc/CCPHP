bbcc2fa63eb4af83c4d40b738581fb2c232c37c6
Ivan Volik <i.a.volik@gmail.com>
2019-02-18 01:59:34 +0300

    #63 Перевел всё, кроме рентал полиси

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,7 @@ export class HomeComponent implements OnInit {
   searchForm: FormGroup;
   times:string[] = ["01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00","11:00","12:00","13:00","14:00",
   "15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","23:50"];
-  locations:string[] = ['Аэропорт Ираклиона','Андреа Папандреу','Херсонисос'];
+  locations:string[] = ['AIR_HER','AN_PAPAN','HERSONISOS'];
   // tslint:disable-next-line:whitespace
   search:Search = new Search();
   workCoords:number;
